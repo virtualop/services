fa4029c29dec3d2978c7c0231299ab5e301584b0
Philipp T <philipp@virtualop.org>
2014-02-23 10:39:37 +0100

    2nd attempt: only set a mysql password if one is configured

--- standard/my_sql/commands/my_sql_install.rb
+++ standard/my_sql/commands/my_sql_install.rb
@@ -1,14 +1,5 @@
 param :machine
 
 on_machine do |machine, params|
-  begin
-    mysql_user = config_string('mysql_user', '')
-    if mysql_user == 'root'
-      new_password = config_string('mysql_password', '')
-      if new_password != ''
-        machine.ssh "mysqladmin -u root password #{new_password}"
-      end
-    end
-  rescue
-  end   
+     
 end
--- standard/my_sql/services/my_sql.rb
+++ standard/my_sql/services/my_sql.rb
@@ -11,8 +11,13 @@ post_first_start do |machine, params|
   begin
     # TODO we can do this only once
     # TODO hardcoded password
-    new_password = 'the_password'
-    machine.ssh("command" => "mysqladmin -u root password #{new_password}")
+    mysql_user = config_string('mysql_user', '')
+    if mysql_user == 'root'
+      new_password = config_string('mysql_password', '')
+      if new_password != ''
+        machine.ssh "mysqladmin -u root password #{new_password}"
+      end
+    end
   rescue
   end
 end
