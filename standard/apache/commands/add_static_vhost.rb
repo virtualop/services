e7f6bc8c66f0fc892d916a641947be8157d8b001
Philipp T <philipp@virtualop.org>
2014-04-10 17:39:15 +0200

    twist_target

--- standard/apache/commands/add_static_vhost.rb
+++ standard/apache/commands/add_static_vhost.rb
@@ -4,8 +4,11 @@ param :machine
 
 param! "server_name", "the http domain served by this vhost", :allows_multiple_values => true
 param! "document_root", "fully qualified path to the directory holding the static files"
-param "twist", "some extra content that should be included in the Directory section of the generated config"
+
+param "twist", "extra content that should be included in the Directory section of the generated config"
 param "erb_twist", "like +twist+, but gets erb processed (probably not a good idea (tm))"
+param "twist_target", "can be used to add +twist+/+erb_twist+ into the VHost rather than the Directory section",
+  :lookup_method => lambda { %w|vhost directory| }, :default_value => 'directory'
 
 as_root do |machine, params|
   generated_dir = machine.apache_generated_conf_dir
@@ -13,13 +16,20 @@ as_root do |machine, params|
   # TODO handle other domains
   first_domain = params["server_name"].first
   
-  directory_includes = ''
-  vhost_includes = ''
+  includes = {
+    :vhost => '',
+    :directory => ''
+  }
   
+  twist = nil
   if params['twist']
-    vhost_includes = params["twist"]  
+    twist = params["twist"]  
   elsif params['erb_twist']
-    vhost_includes = output = ERB.new(params["erb_twist"]).result(binding())
+    twist = ERB.new(params['erb_twist']).result(binding())
+  end
+  
+  if twist
+    includes[params['twist_target'].to_sym] = twist
   end
   
   process_local_template(:apache_static_vhost, machine, "#{generated_dir}/#{first_domain}.conf", binding())
