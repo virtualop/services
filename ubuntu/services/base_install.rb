7e45b42b4410b2d3c6135b355f441698b2509e97
Philipp T <philipp@virtualop.org>
2018-03-25 20:12:05 +0200

    fix for inquisitive apt-get upgrade, host_install

--- ubuntu/services/base_install.rb
+++ ubuntu/services/base_install.rb
@@ -3,7 +3,8 @@ deploy do |machine|
   # TODO set the domain as well?
 
   machine.sudo "apt-get update"
-  machine.sudo "apt-get upgrade -y"
+  # thanks https://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt#answer-147079
+  machine.sudo "DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' upgrade -y"
 
   machine.install_package "apt-transport-https"
 end
