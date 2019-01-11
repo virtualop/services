400018822fdfe279e59723eebbd5de37f306f91e
Philipp T <philipp@hitchhackers.net>
2019-01-11 15:10:20 +0100

    base_dir and vop_user params for vop.dev

--- vop/services/dev.rb
+++ vop/services/dev.rb
@@ -1,4 +1,5 @@
-binary_name "./vop/bin/vop.sh"
+process_regex [ /vop.sh/, /puma/, /sidekiq/ ]
+
 icon "vop_16px.png"
 
 deploy package: %w|ruby ruby-dev ruby-bundler| +
@@ -8,37 +9,47 @@ deploy package: %w|ruby ruby-dev ruby-bundler| +
 # web dependencies
 deploy package: %w|libsqlite3-dev zlib1g-dev nodejs|
 
-# TODO make all checkout locations relative to a BASE_DIR?
-
-%w|vop plugins services web|.each do |repo|
-  deploy github: "virtualop/#{repo}"
-end
-
-param! "domain"
+param "domain"
+param "base_dir", default: "virtualop"
+param "vop_user", default: "marvin"
 
 deploy do |machine, params|
-  [ "vop", "web" ].each do |dir|
-    machine.ssh "cd #{dir} && bundle install"
+  base_dir = params["base_dir"]
+  unless base_dir.start_with? "/"
+    base_dir = "#{machine.home}/#{base_dir}"
+  end
+  $logger.info "installing vop.dev into #{base_dir}"
+
+  %w|vop plugins services web|.each do |repo|
+    machine.deploy_from_github(
+      "github_project" => "virtualop/#{repo}",
+      "dir" => "#{base_dir}/#{repo}"
+    )
+  end
+
+  [ "vop", "web" ].each do |repo|
+    path = "#{base_dir}/#{repo}"
+    machine.ssh "cd #{path} && bundle install"
   end
 
   machine.write_systemd_config(
     "name" => "vop-web",
     "user" => "marvin",
-    "exec_start" => "#{machine.home}/web/bin/web.sh",
+    "exec_start" => "#{base_dir}/web/bin/web.sh",
     "after" => "redis.service"
   )
 
   machine.write_systemd_config(
     "name" => "vop-background",
     "user" => "marvin",
-    "exec_start" => "#{machine.home}/vop/bin/sidekiq.sh",
+    "exec_start" => "#{base_dir}/vop/bin/sidekiq.sh",
     "after" => "redis.service"
   )
 
   machine.write_systemd_config(
     "name" => "vop-message-pump",
     "user" => "marvin",
-    "exec_start" => "#{machine.home}/web/bin/message-pump.sh",
+    "exec_start" => "#{base_dir}/web/bin/message-pump.sh",
     "after" => "redis.service"
   )
 
@@ -49,27 +60,31 @@ deploy do |machine, params|
 
   machine.install_service("apache.reverse_proxy")
 
-  machine.add_reverse_proxy(
-    server_name: params["domain"],
-    target_url: "http://localhost:3000/"
-  )
-  machine.parent.reverse_proxy.add_reverse_proxy(
-    server_name: params["domain"],
-    target_url: "http://#{machine.internal_ip}/"
-  )
+  cable_domain = nil
+  if params["domain"]
+    machine.add_reverse_proxy(
+      server_name: params["domain"],
+      target_url: "http://localhost:3000/"
+    )
+    machine.parent.reverse_proxy.add_reverse_proxy(
+      server_name: params["domain"],
+      target_url: "http://#{machine.internal_ip}/"
+    )
 
-  cable_domain = "cable.#{params["domain"]}"
-  machine.add_reverse_proxy(
-    server_name: cable_domain,
-    target_url: "ws://localhost:3000/cable/"
-  )
-  machine.parent.reverse_proxy.add_reverse_proxy(
-    server_name: cable_domain,
-    target_url: "ws://#{machine.internal_ip}/cable/"
-  )
+    cable_domain = "cable.#{params["domain"]}"
+    machine.add_reverse_proxy(
+      server_name: cable_domain,
+      target_url: "ws://localhost:3000/cable/"
+    )
+    machine.parent.reverse_proxy.add_reverse_proxy(
+      server_name: cable_domain,
+      target_url: "ws://#{machine.internal_ip}/cable/"
+    )
+  end
 
   machine.vop_init(
     "vop_domain" => params["domain"],
-    "cable_domain" => cable_domain
+    "cable_domain" => cable_domain,
+    "vop_user" => params["vop_user"]
   )
 end
