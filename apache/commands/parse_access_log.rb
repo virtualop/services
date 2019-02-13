cf714249787b3e3f18c8520171dc936c29d6d4a3
Philipp T <philipp@hitchhackers.net>
2019-02-13 17:09:32 +0100

    allow log entries with Basic Auth user

--- apache/commands/parse_access_log.rb
+++ apache/commands/parse_access_log.rb
@@ -1,6 +1,6 @@
 param! "data", "the access log lines that should be read", multi: true
 
-show columns: %w|remote_ip, timestamp, request, status|
+show columns: %w|remote_ip timestamp request status|
 
 run do |data|
   data.map do |line|
@@ -13,7 +13,7 @@ run do |data|
 
     # 192.168.122.75 - - [01/Jul/2018:17:42:11 +0200] "GET /assets/footer.self-93db14d7dcc90f201c632e9a5f26d7e725599a4201214d7a1bf79539c16aa84b.js?body=1 HTTP/1.1" \
     #   200 711 "http://vop31.santafe.virtualop.org/" "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"
-    result = /([\d\.]+)\s+(-)\s+(-)\s+\[([^\]]+)\]\s+\"([^\"]+)\"\s+(\d{3})\s+(\d+)\s+\"([^\"]+)\"\s+\"([^\"]+)\"/.match(line)
+    result = /([\d\.]+)\s+(\S+)\s+(\S+)\s+\[([^\]]+)\]\s+\"([^\"]+)\"\s+(\d{3})\s+(\d+)\s+\"([^\"]+)\"\s+\"([^\"]+)\"/.match(line)
     if result
       entry = {
         remote_ip: result.captures[0],
@@ -34,6 +34,8 @@ run do |data|
       else
         $logger.warn("unexpected timestamp format: '#{apache_timestamp}'")
       end
+    else
+      # TODO maybe count unparseable lines?
     end
 
     entry
